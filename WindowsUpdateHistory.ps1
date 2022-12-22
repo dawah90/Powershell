7f79ce7bd97ce6e0fb87d2239a62ca07c02a96de
Daniel Wahlfridsson <43478449+dawah90@users.noreply.github.com>
2022-12-22 16:13:26 +0100

    Create WindowsUpdateHistory.ps1

---
+++ WindowsUpdateHistory.ps1
@@ -0,0 +1,19 @@
+$DateNow = Get-Date -Format "HH-mm"
+Start-Transcript -Path C:\Temp\2022-12-22-$DateNow.txt
+
+if (Get-PackageProvider | Where-Object { $_.Name -eq "nuget" }) {
+    Write-Output "Nuget already installed" }
+    else {
+    Write-Output "Installing Nuget..."
+    Install-PackageProvider nuget -force
+    }
+
+if (Get-Module | Where-Object { $_.Name -eq "PSWindowsUpdate" }) {
+    Write-Output "PSWindowsUpdate Already installed" }
+    else {
+    Write-Output "Installing Module PSWindowsUpdate..."
+    Install-Module -Name PSWindowsUpdate -Force
+    }
+
+$WUHistory = Get-WUHistory -Last 100
+$WUHistory | Where-Object { $_.Result -eq "Succeeded" -and $_.KB -ne "KB2267602" -and $_.KB.Length -gt 0 } | format-table -Property Date, KB, Title
