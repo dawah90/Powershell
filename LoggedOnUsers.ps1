0e1fd3001f1e56dca9fec6549cea1c92e2322698
Daniel Wahlfridsson <43478449+dawah90@users.noreply.github.com>
2022-12-22 16:27:19 +0100

    Update LoggedOnUsers.ps1

--- LoggedOnUsers.ps1
+++ LoggedOnUsers.ps1
@@ -1,2 +1,5 @@
 $LoggedOnUsers = Get-WMIObject -Class Win32_ComputerSystem | Select-Object UserName
 Write-Output $LoggedOnUsers.UserName
+
+#Get all Objects
+Get-WMIObject -Class Win32_ComputerSystem | Get-Member
