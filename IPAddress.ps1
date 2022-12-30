41ac3ee1cf5d6e0b72a7ed4caf80d50b29b0f1e1
Daniel Wahlfridsson <43478449+dawah90@users.noreply.github.com>
2022-12-30 10:21:18 +0100

    Create IPAddress.ps1

---
+++ IPAddress.ps1
@@ -0,0 +1,3 @@
+#$NetAdapter = Get-NetAdapter | Format-list
+$NetworkCard = Get-NetIPAddress | Where-Object {$_.AddressFamily -eq "IPv4" -and $_.InterfaceAlias -eq "Ethernet"}
+$IPAddress = $NetworkCard.IPAddress
