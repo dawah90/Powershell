3b587231c95cb1de709bf99ca92bd4dee4affadd
Daniel Wahlfridsson <43478449+dawah90@users.noreply.github.com>
2022-12-30 21:52:55 +0100

    Small changes

--- ChocolateyDownload.ps1
+++ ChocolateyDownload.ps1
@@ -1,5 +1,5 @@
 if (-not (test-path "C:\ProgramData\chocolatey\bin\choco.exe")){
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
     choco feature enable -n allowGlobalConfirmation
     choco feature disable -n showdownloadprogress
     Clear-Host
--- IPAddress.ps1
+++ IPAddress.ps1
@@ -1,3 +1,5 @@
 #$NetAdapter = Get-NetAdapter | Format-list
 $NetworkCard = Get-NetIPAddress | Where-Object {$_.AddressFamily -eq "IPv4" -and $_.InterfaceAlias -eq "Ethernet"}
 $IPAddress = $NetworkCard.IPAddress
+$IPAddress
+
