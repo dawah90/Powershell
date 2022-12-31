a8dcb9181909f78655e1e3f6de923553e9264f5a
Daniel Wahlfridsson <43478449+dawah90@users.noreply.github.com>
2022-12-31 14:36:25 +0100

    Update Transcription.ps1
    Added conditions that only creates the transcript file if it doesn't already exists

--- Transcription.ps1
+++ Transcription.ps1
@@ -4,5 +4,11 @@ if (-not (Test-Path -path $LogLocation)) {
     New-Item $LogLocation -ItemType Directory
 }
 
-$DateNow = Get-Date -UFormat "%Y-%m-%d-%H-%M-%S"
-Start-Transcript -Path "$logLocation\Powershell-$DateNow.log"
+if (-not ($DateNow)) {
+    $DateNow = Get-Date -UFormat "%Y-%m-%d-%H-%M-%S"
+    $Transcript = "$logLocation\Powershell-$DateNow.log"
+} 
+
+if (-not (Test-Path -path $Transcript)) {
+    Start-Transcript -Path $Transcript
+}
