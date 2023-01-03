00fc3f034eb2d07f448c538b10c67f08da5a5077
Daniel Wahlfridsson <43478449+dawah90@users.noreply.github.com>
2023-01-03 15:02:49 +0100

    Create Networking.ps1

---
+++ Networking.ps1
@@ -0,0 +1,3 @@
+Test-NetConnection -ComputerName <remote device> -Port <port number>
+Test-NetConnection -ComputerName 172.22.2.83 -Port 2638
+Test-NetConnection -ComputerName 10.239.1.78 -Port 2638
