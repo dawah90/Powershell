54c0c0acfe02c1670da8e6b7e235381602b85c24
Daniel Wahlfridsson <43478449+dawah90@users.noreply.github.com>
2023-01-04 17:54:30 +0100

    Update Get-Locale.ps1

--- Get-Locale.ps1
+++ Get-Locale.ps1
@@ -1,17 +1,11 @@
 [System.Globalization.NumberFormatInfo]::CurrentInfo.NumberDecimalSeparator
-
-#[System.Globalization.CultureInfo]::GetCultureInfo("culture code").NumberFormat.NumberDecimalSeparator
-
 [System.Globalization.CultureInfo]::GetCultureInfo("en-US").NumberFormat.NumberDecimalSeparator
 
 Pause
 
+#Set Culture Info to swedish format. Makes sure decimal delimiter is shown as , and not .
 [System.Threading.Thread]::CurrentThread.CurrentCulture = [System.Globalization.CultureInfo]::GetCultureInfo("sv-SE")
-
 [System.Globalization.NumberFormatInfo]::CurrentInfo.NumberDecimalSeparator
-
-#[System.Globalization.CultureInfo]::GetCultureInfo("culture code").NumberFormat.NumberDecimalSeparator
-
 [System.Globalization.CultureInfo]::GetCultureInfo("en-US").NumberFormat.NumberDecimalSeparator
 
 Pause
