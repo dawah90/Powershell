080c98f434325d70bdf47debecaaedad61a6a049
Daniel Wahlfridsson <43478449+dawah90@users.noreply.github.com>
2023-01-07 11:26:16 +0100

    Update CompareLists.ps1

--- CompareLists.ps1
+++ CompareLists.ps1
@@ -1,20 +1,26 @@
-﻿# Define the two lists
-$list1 = 1, 2, 3, 4, 5
-$list2 = 3, 4, 5, 6, 7
+﻿#Compare 2 AD-users group membership
+
+$User1 = Read-Host "Enter user 1"
+$User2 = Read-Host "Enter user 2"
+
+$list1 = Get-ADPrincipalGroupMembership -Identity $User1 | Select-Object -Property name
+$list2 = Get-ADPrincipalGroupMembership -Identity $User2 | Select-Object -Property name
+$list1Names = $list1.name
+$list2Names = $list2.name
 
 # Find the items that are common to both lists
-$common = $list1 | Where-Object {$list2 -contains $_}
+$common = $list1Names | Where-Object {$list2Names -contains $_}
 
 # Find the items that are unique to each list
-$unique1 = $list1 | Where-Object {$list2 -notcontains $_}
-$unique2 = $list2 | Where-Object {$list1 -notcontains $_}
+$unique1 = $list1Names | Where-Object {$list2Names -notcontains $_}
+$unique2 = $list2Names | Where-Object {$list1Names -notcontains $_}
 
-# Create the table
-$table = @{
-    "Unique to list 1" = $unique1
-    "Common" = $common
-    "Unique to list 2" = $unique2
-}
-
-# Output the table
-$table | Format-Table
+Write-Output " "
+Write-Host "Common for both users" -ForegroundColor Cyan
+Write-Output $common
+Write-Output " "
+Write-Host "Unique for $user1" -ForegroundColor Cyan
+Write-Output $unique1
+Write-Output " "
+Write-Host "Unique for $user2" -ForegroundColor Cyan
+Write-Output $unique2
