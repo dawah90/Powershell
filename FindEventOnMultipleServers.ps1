48d8541aa5cc033d19b2f21f799aa53519c09c4b
Daniel Wahlfridsson <43478449+dawah90@users.noreply.github.com>
2023-01-23 13:32:41 +0100

    Update FindEventOnMultipleServers.ps1

--- FindEventOnMultipleServers.ps1
+++ FindEventOnMultipleServers.ps1
@@ -1,3 +1,9 @@
-$User = "UserName"
-$WinEvents = Invoke-Command -ComputerName bmards002,bmards003,bmards004,bmards005,bmards006 -Scriptblock { Get-WinEvent -LogName "Microsoft-Windows-TerminalServices-LocalSessionManager/Operational" }
-$WinEvents | Where-Object {$_.ID -eq "21" -and $_.Message -like "*$user*"} | Sort-Object TimeCreated | Format-List
+#Example 1 - Everything to console 
+#$User = "UserName"
+#$WinEvents = Invoke-Command -ComputerName bmards002,bmards003,bmards004,bmards005,bmards006 -Scriptblock { Get-WinEvent -LogName "Microsoft-Windows-TerminalServices-LocalSessionManager/Operational" }
+#$WinEvents | Where-Object {$_.ID -eq "21" -and $_.Message -like "*$user*"} | Sort-Object TimeCreated | Format-List
+
+#Example 2 - Selected values to log file
+#$User = "UserName"
+#$WinEvents = Invoke-Command -ComputerName bmards002,bmards003,bmards004,bmards005,bmards006 -Scriptblock { Get-WinEvent -LogName "Microsoft-Windows-TerminalServices-LocalSessionManager/Operational" }
+#$WinEvents | Where-Object {$_.ID -eq "21" -and $_.Message -like "*$user*"} | Sort-Object TimeCreated | Format-List -Property TimeCreated, MachineName, Message | Out-File -FilePath "C:\temp\PowerShellOutput.log" -append
