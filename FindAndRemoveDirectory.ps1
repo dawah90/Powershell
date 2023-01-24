a46a13be55944647210c02ee6830d6e23adce249
Daniel Wahlfridsson <43478449+dawah90@users.noreply.github.com>
2023-01-24 10:45:43 +0100

    Update FindAndRemoveDirectory.ps1

--- FindAndRemoveDirectory.ps1
+++ FindAndRemoveDirectory.ps1
@@ -1,5 +1,6 @@
 $FolderToBeDeleted = "C:\Program Files (x86)\HT2000w.9x\SETUP\BDEA"
 
 if (Test-Path -path $FolderToBeDeleted) {
-  write-output "folder found"
+  write-output "folder found. Removing folder"
+  Remove-item -Path $FolderToBeDeleted -Recurse -Force
 } 
