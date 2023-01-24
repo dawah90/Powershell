9e70a8b03f7e2ef091f586f33a65656507a9ae30
Daniel Wahlfridsson <43478449+dawah90@users.noreply.github.com>
2023-01-24 18:04:30 +0100

    Update FindEventOnMultipleServers.ps1

--- FindEventOnMultipleServers.ps1
+++ FindEventOnMultipleServers.ps1
@@ -7,3 +7,15 @@
 #$User = "UserName"
 #$WinEvents = Invoke-Command -ComputerName bmards002,bmards003,bmards004,bmards005,bmards006 -Scriptblock { Get-WinEvent -LogName "Microsoft-Windows-TerminalServices-LocalSessionManager/Operational" }
 #$WinEvents | Where-Object {$_.ID -eq "21" -and $_.Message -like "*$user*"} | Sort-Object TimeCreated | Format-List -Property TimeCreated, MachineName, Message | Out-File -FilePath "C:\temp\PowerShellOutput.log" -append
+
+#Example 3 - Selected text from message with specific time format
+$User = "UserName"
+$WinEvents = Invoke-Command -ComputerName bmards002,bmards003,bmards004,bmards005,bmards006 -Scriptblock { Get-WinEvent -LogName "Microsoft-Windows-TerminalServices-LocalSessionManager/Operational" }
+$FoundEvents = $WinEvents | Where-Object {$_.ID -eq "21" -and $_.Message -like "*$user*"} | Sort-Object TimeCreated
+
+Foreach ($FoundEvent in $FoundEvents) {
+    Write-Output ""
+    Write-Output $FoundEvent.Message.Substring(59,12)
+    Write-Output $FoundEvent.TimeCreated.ToString("yyyy-MM-dd - HH:mm")
+    Write-Output $FoundEvent.MachineName
+}
