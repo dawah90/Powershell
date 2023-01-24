c369d61b917609211468bce567bf98bc501f4901
Daniel Wahlfridsson <43478449+dawah90@users.noreply.github.com>
2023-01-24 10:50:24 +0100

    Update FindAndRemoveDirectory.ps1

--- FindAndRemoveDirectory.ps1
+++ FindAndRemoveDirectory.ps1
@@ -1,6 +1,8 @@
 $FolderToBeDeleted = "C:\Program Files (x86)\HT2000w.9x\SETUP\BDEA"
+Get-Process | Where-Object Name -eq "Startht4" | Stop-Process -Force
 
 if (Test-Path -path $FolderToBeDeleted) {
   write-output "folder found. Removing folder"
   Remove-item -Path $FolderToBeDeleted -Recurse -Force
 } 
+
