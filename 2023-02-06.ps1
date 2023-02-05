133ee049390f27e1fca0db1ffec76da6f51b9231
Daniel Wahlfridsson <43478449+dawah90@users.noreply.github.com>
2023-02-05 23:31:08 +0100

    Create 2023-02-06.ps1

---
+++ 2023-02-06.ps1
@@ -0,0 +1,62 @@
+Get-Process | `
+Where-Object Name -eq "svchost" | `
+#Select-Object * | `
+Sort-Object HandleCount -Descending | `
+Format-Table -Property Name, HandleCount, StartTime
+
+##
+<#
+Get-NetAdapter | `
+Where-Object Status -eq "Up" | `
+Select-Object Name, ifindex | `
+Format-List
+
+$Adapterinfo = Get-NetAdapter | `
+Where-Object Status -eq "Up"
+
+foreach ($adapter in $Adapterinfo) {
+    $ActiveLivingAdapter = Get-NetIPAddress -InterfaceIndex $adapter.ifIndex | Where-Object {$_.AddressFamily -eq "IPv4" -and $_.ValidLifeTime -lt "1"}
+    $ActiveLivingAdapter.IPAddress
+}
+#>
+
+$adapter = Get-NetAdapter | `
+Where-Object Status -eq "Up"
+
+$correctadapter = Get-NetIPAddress -InterfaceIndex $adapter.ifIndex | `
+Where-Object {$_.AddressFamily -eq "IPv4" -and $_.ValidLifeTime -lt "1"}
+
+$correctadapterindex = $correctadapter.InterfaceIndex
+
+Set-NetIPInterface -InterfaceIndex $correctadapterindex -DHCP Disabled -PassThru
+
+New-NetIPAddress `
+ -AddressFamily IPv4 `
+ -InterfaceIndex $correctadapterindex `
+ -IPAddress 192.168.50.20 `
+ -PrefixLength 24 `
+ -DefaultGateway 192.168.50.1
+
+ Set-DnsClientServerAddress `
+ -InterfaceIndex $correctadapterindex `
+ -ServerAddresses 8.8.8.8,8.8.4.4
+
+$adaptername = $adapter.Name
+$adapterindex = $adapter.ifindex
+
+$VersionCodes = @"
+
+Multiple
+Lines
+AdapterNames: $adaptername
+AdapterIndexes: $adapterindex
+Of
+Information
+
+"@
+$VersionCodes
+
+
+
+
+
