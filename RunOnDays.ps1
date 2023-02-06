7c59887118f39c3879c22655cbc7273ede7fa7b5
Daniel Wahlfridsson <43478449+dawah90@users.noreply.github.com>
2023-02-06 21:30:43 +0100

    Create RunOnDays.ps1

---
+++ RunOnDays.ps1
@@ -0,0 +1,8 @@
+$datetime=[datetime]::Today
+if($datetime.DayOfWeek -match 'Monday|Wednesday|Friday'){
+    Write-Output "m-w-f"
+}elseif($datetime.DayOfWeek -match 'Tuesday|Thursday|Saturday'){
+    Write-Output "t-t-s"
+}else{
+    Write-Output "its sunday"
+}
