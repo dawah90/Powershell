89ce7e454725bcfcf804f7e3a8f4b645666a0df1
Daniel Wahlfridsson <43478449+dawah90@users.noreply.github.com>
2023-02-09 20:59:08 +0100

    Update 2023-02-06.ps1

--- 2023-02-06.ps1
+++ 2023-02-06.ps1
@@ -59,7 +59,44 @@ Information
 "@
 $VersionCodes
 
+Get-Process | Select-Object -first 1 | format-list -property *
 
+ Get-Process | Select-Object -last 5 | Format-Table -auto `
+ @{label="Application"; Expression={$_.Name}; Width=10; Align="Right" }, `
+ @{label="Handles"; Expression={$_.HandleCount}; Align="Center"}, `
+ @{label="Application Path"; Expression={$_.Path} `
+}
+ 
+
+$RDPRules = Get-NetFirewallRule | Where-Object {$_.DisplayName -match "Remote Desktop" }
+$FPRules = Get-NetFirewallRule | Where-Object {$_.DisplayName -match "File and Printer Sharing" }
+$WMIRules = Get-NetFirewallRule | Where-Object {$_.DisplayName -match "Windows Management Instrumentation" }
+
+ForEach ($rule in $RDPRules,$FPRules,$WMIRules ) {
+    Write-Output $rule.Count
+# Set-NetFirewallRule -DisplayName $rule.DisplayName `
+# -Direction Inbound `
+# -Profile Domain,Private `
+# -Action Allow `
+# -Enabled True `
+# -PassThru
+}
+
+$Rules = Get-NetFirewallRule | Where-Object {$_.DisplayName -match "Remote Desktop|File and Printer Sharing|Windows Management Instrumentation" }
+
+ForEach ($rule in $Rules) {
+    Write-Output $rule
+}
+
+#Enable-PSRemoting -Force
+
+#RFC1918
+#10.0.0.0/8
+#10.0.0.0        -   10.255.255.255
+#172.16.0.0/12
+#172.16.0.0      -   172.31.255.255
+#192.168.0.0/16 
+#192.168.0.0     -   192.168.255.255
 
 
 
