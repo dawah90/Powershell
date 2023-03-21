1c04aa98f7497f4faaf95b5026418462ad42c725
Daniel Wahlfridsson <43478449+dawah90@users.noreply.github.com>
2023-03-21 19:09:50 +0100

    Update Command-Confirm-Impact.ps1

--- Command-Confirm-Impact.ps1
+++ Command-Confirm-Impact.ps1
@@ -1,21 +1,48 @@
+#Source
 https://renenyffenegger.ch/notes/Windows/PowerShell/language/variable/preference/confirmPreference
-
-$stopProcMeta = new-object System.Management.Automation.CommandMetadata(get-command stop-process)
-$stopProcMeta.ConfirmImpact
 
 #####
 
-$Commands = Get-Command | Select-Object -first 50
-$ImpactHash = @{}
-$ErrorLog = "C:\temp\ConfirmImpactError.log"  
+#Add available commands to variable
+$Commands = Get-Command
 
-foreach ($Command in $Commands) {    
-$MetaData = new-object System.Management.Automation.CommandMetadata(get-command $Command)    
-  Try {        
-    $ImpactHash.Add("$($Command.Name)","$($MetaData.ConfirmImpact)")    
-  } Catch {        
-    Add-Content -Value $Error[0] -Path $ErrorLog    
+#Create empty array
+$CommandImpactArray = @()
+
+#Oterate through each command
+foreach ($Command in $Commands) {
+    
+    #Try and catch for errors
+    try {
+        #Get command metadata like ConfirmImpact
+        $MetaData = new-object System.Management.Automation.CommandMetadata($Command)
+    } catch {
+        write-host "error!! - $Command" -ForegroundColor Red
     }
-}  
-$ImpactHash
+  
+    #Create hash object
+    $CommandImpactHash = [ordered]@{
+        Name        = $Command.Name
+        ImpactLevel = $MetaData.ConfirmImpact
+    }
+  
+    #Turn hash object into custom object
+    $CustomObjectFromHash = New-Object -TypeName PSObject -Property $CommandImpactHash
+    
+    #Add custom object to array
+    $CommandImpactArray += $CustomObjectFromHash
+}
+
+#Parse info into separate lists
+$NoneImpactCommands = $CommandImpactArray | Where-Object ImpactLevel -eq "none"
+$LowImpactCommands = $CommandImpactArray | Where-Object ImpactLevel -eq "low"
+$MediumImpactCommands = $CommandImpactArray | Where-Object ImpactLevel -eq "medium"
+$HighImpactCommands = $CommandImpactArray | Where-Object ImpactLevel -eq "high"
+
+#Present sorted lists
+$LowImpactCommands | Sort-Object Name
+$HighImpactCommands | Sort-Object Name
+$NoneImpactCommands | Sort-Object Name
+
+
 
