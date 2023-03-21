d4d60926e20e94fdbf86d6a252b1e568d5320d2d
Daniel Wahlfridsson <43478449+dawah90@users.noreply.github.com>
2023-03-21 11:13:20 +0100

    Create Hashtable.ps1

---
+++ Hashtable.ps1
@@ -0,0 +1,17 @@
+#Hash
+$hash = @{
+    'person' = 'x'
+    'datum' = get-date
+    'tid' = 'nu'
+    'djur = 'apa'
+    'dator' = $env:COMPUTERNAME
+}
+
+#OrderedHash
+$hash = [ordered]@{
+    'person' = 'x'
+    'datum' = get-date
+    'tid' = 'nu'
+    'djur = 'apa'
+    'dator' = $env:COMPUTERNAME
+}
