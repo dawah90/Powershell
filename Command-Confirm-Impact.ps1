fc6a2b9e43bac948418bb7839d0ca74f52aee547
Daniel Wahlfridsson <43478449+dawah90@users.noreply.github.com>
2023-03-21 15:11:34 +0100

    Update Command-Confirm-Impact.ps1

--- Command-Confirm-Impact.ps1
+++ Command-Confirm-Impact.ps1
@@ -2,3 +2,18 @@ https://renenyffenegger.ch/notes/Windows/PowerShell/language/variable/preference
 
 $stopProcMeta = new-object System.Management.Automation.CommandMetadata(get-command stop-process)
 $stopProcMeta.ConfirmImpact
+
+$Commands = Get-Command | Select-Object 50
+$ImpactHash = @{}
+$ErrorLog = "C:\temp\ConfirmImpactError.log"  
+
+foreach ($Command in $Commands) {    
+$MetaData = new-object System.Management.Automation.CommandMetadata(get-command $Command)    
+  Try {        
+    $ImpactHash.Add("$($Command.Name)","$($MetaData.ConfirmImpact)")    
+  } Catch {        
+    Add-Content -Value $Error[0] -Path $ErrorLog    
+    }
+}  
+$ImpactHash
+
