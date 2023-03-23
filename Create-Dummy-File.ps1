6880ae4d9aaea56752789b0ec62651d491d39f7f
Daniel Wahlfridsson <43478449+dawah90@users.noreply.github.com>
2023-03-23 15:49:04 +0100

    Update Create-Dummy-File.ps1

--- Create-Dummy-File.ps1
+++ Create-Dummy-File.ps1
@@ -1,2 +1,2 @@
-#Create dummy file
-(New-Object System.IO.FileStream "$pwd\file.txt", Create, ReadWrite).SetLength(100MB)
+#Create dummy file, Set size to 1GB and close file.
+(New-Object System.IO.FileStream "$pwd\file.txt", 'Create', 'ReadWrite') | ForEach-Object { $_.SetLength(1GB); $_.Close() }
